29305b56661953ccb6be5533ddfc220c3f9c09de
Karim Ahmed <karimib123@hotmail.com>
2024-06-10 20:12:10 -0400

    use github.com/shirou/gopsutil to retrieve system information

--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
+)
 
+func systemInfo() {
+	//runTimeOS := runtime.GOOS
+	hostStat, _ := host.Info()
+	fmt.Println("operating system", hostStat.OS)
+	fmt.Println("platform ", hostStat.Platform)
+	fmt.Println("hostname ", hostStat.Hostname)
+	fmt.Println("num of processes", hostStat.Procs)
+
+	memory, _ := mem.VirtualMemory()
+	fmt.Println("memory", memory.Total)
+	fmt.Println("memory ", memory.Free)
+	fmt.Println("used ", memory.UsedPercent, "%")
+}
+func diskInfo() {
+	diskStat, _ := disk.Usage("\\")
+	fmt.Println("mem", diskStat.Total)
+	fmt.Println("mem", diskStat.Free)
+	fmt.Println("mem", diskStat.UsedPercent, "%")
+}
+func cpuInfo() {
+	cpuStat, _ := cpu.Info()
+	var index = 0
+	for index < len(cpuStat) {
+		fmt.Println("CPU [", index, "]", cpuStat[index].Cores)
+		index++
+	}
+}
 func main() {
-	variable := 5
-	fmt.Println(variable)
+	//hostStat, _ := host.Info()
+	//a := Test1{1}
+	//fmt.Println(hostStat.Hostname)
+	systemInfo()
+	diskInfo()
+	cpuInfo()
 }
